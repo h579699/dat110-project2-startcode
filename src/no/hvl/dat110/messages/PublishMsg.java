f47ec5b7cce199b7c38191ee7b8d98b12a0b80a3
jjona <jjona@LAPTOP-P2I2K30C>
2021-02-17 09:56:01 +0100

    Task A

--- src/no/hvl/dat110/messages/PublishMsg.java
+++ src/no/hvl/dat110/messages/PublishMsg.java
@@ -8,12 +8,51 @@ public class PublishMsg extends Message {
 
 	// TODO:
 	// Implement object variables - a topic and a message is required
+	private String topic;
+	private String message;
 
 	// Constructor, get/set-methods, and toString method
 	// as described in the project text
-	
-	public String getMessage() {
-		
-		throw new UnsupportedOperationException(TODO.method());
+	public PublishMsg(String user, String topic, String message) {
+		super(MessageType.PUBLISH, user);
+		this.topic = topic;
+		this.message = message;
 	}
+
+	/**
+	 * @return the topic
+	 */
+	public String getTopic() {
+		return topic;
+	}
+
+	/**
+	 * @param topic the topic to set
+	 */
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+
+	/**
+	 * @return the message
+	 */
+	public String getMessage() {
+		return message;
+	}
+
+	/**
+	 * @param message the message to set
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "PublishMsg [topic=" + topic + ", message=" + message + "]" + super.toString();
+	}
+	
+	
+	
+	
 }
